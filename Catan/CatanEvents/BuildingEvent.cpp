6c8cf29473b0b2ee7bd221dcbade81165156cc70
Lucas Kammann <lucaskammann@hotmail.com>
2018-11-28 07:00:26 -0300

    Buildings forcing prevention

--- Catan/CatanEvents/BuildingEvent.cpp
+++ Catan/CatanEvents/BuildingEvent.cpp
@@ -4,7 +4,17 @@ BuildingEvent::
 BuildingEvent(BuildingPacket* packet) : CatanEvent(Events::BUILDING, Sources::NETWORKING, PlayerId::PLAYER_TWO), BuildingData(packet->getType(), packet->getCoords()) {}
 
 BuildingEvent::
-BuildingEvent(Coord coords, BuildingType type) : CatanEvent(Events::BUILDING, Sources::GUI, PlayerId::PLAYER_ONE), BuildingData(type, coords) {}
+BuildingEvent(Coord coords, BuildingType type) : CatanEvent(Events::BUILDING, Sources::GUI, PlayerId::PLAYER_ONE), BuildingData(type, coords) {
+	switch (type) {
+		case BuildingType::CITY:
+		case BuildingType::SETTLEMENT:
+			this->coords.forceDot();
+			break;
+		case BuildingType::ROAD:
+			this->coords.forceEdge();
+			break;
+	}
+}
 
 BuildingEvent::
 ~BuildingEvent() {}
