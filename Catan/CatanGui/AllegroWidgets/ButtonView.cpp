e851b4b9ab8d288ad8010c9561a09d0731895850
Joaquin Gaytan <jgaytan@itba.edu.ar>
2018-11-30 12:26:12 -0300

    Adaptados los Views al paradiigma de configs

--- Catan/CatanGui/AllegroWidgets/ButtonView.cpp
+++ Catan/CatanGui/AllegroWidgets/ButtonView.cpp
@@ -4,35 +4,29 @@ void ButtonView::
 draw(void) 
 {
 
-	ALLEGRO_BITMAP* bmap = nullptr;
-	MouseUI* pointer = (MouseUI*)model;
-	switch (pointer->getStatus())
+
+	if(model->getVisible())
 	{
-		case MouseUI::Status::IDLE:
-			bmap = idleBitmap;
-			break;
-
-		case MouseUI::Status::SELECTED:
-		case MouseUI::Status::DRAGGED:
-			bmap = selectedBitmap;
-			break;
-
-		case MouseUI::Status::FOCUSED:
-			bmap = focusedBitmap;
-			break;
-
-
-
+		MouseUI* pointer = (MouseUI*)model;
+		al_draw_rotated_bitmap(images[pointer->getStatus()].bitmap, 
+								al_get_bitmap_width(images[pointer->getStatus()].bitmap) / 2,
+								al_get_bitmap_height(images[pointer->getStatus()].bitmap) / 2,
+								pointer->xPos(), 
+								pointer->yPos(),
+								NO_ROTATION, 
+								NO_FLAGS
+								);
 	}
-	al_draw_rotated_bitmap(bmap, al_get_bitmap_width(bmap) / 2, al_get_bitmap_height(bmap) / 2, pointer->xPos(), pointer->yPos(), NO_ROTATION, NO_FLAGS);
+	
 }
 
 ButtonView::
-ButtonView(ALLEGRO_BITMAP* focused, ALLEGRO_BITMAP* selected, ALLEGRO_BITMAP* idle, WindowUI* interface_, MouseUI* model_) : UIView(interface_, model_)
+ButtonView(WindowUI* interface_, MouseUI* model_) : UIView(interface_, model_)
 {
-	focusedBitmap = focused;
-	selectedBitmap = selected;
-	idleBitmap = idle;
+	images.setConfig(MouseUI::Status::IDLE, DEFAULT_IDLE_BITMAP);
+	images.setConfig(MouseUI::Status::SELECTED, DEFAULT_SELECTED_BITMAP);
+	images.setConfig(MouseUI::Status::FOCUSED, DEFAULT_FOCUSED_BITMAP);
+	images.setConfig(MouseUI::Status::DRAGGED, DEFAULT_DRAGGED_BITMAP);
 
 
 }
--- Catan/CatanGui/AllegroWidgets/TextFieldView.h
+++ Catan/CatanGui/AllegroWidgets/TextFieldView.h
@@ -6,21 +6,27 @@
 #include "../AllegroUI/TextUI.h"
 #include "../AllegroUI/UIView.h"
 
-#define IDLE_COLOR al_map_rgb(255, 255, 255)
-#define SELECTED_COLOR al_map_rgb(0, 255, 0)
-#define FOCUSED_COLOR al_map_rgb(220, 220, 220)
+
 #define PERC_MARGIN 0.1
 
+#define DEFAULT_BACKGROUND_IDLE_COLOR 255,255,255
+#define DEFAULT_BACKGROUND_SELECTED_COLOR 0,255,0
+#define DEFAULT_BACKGROUND_FOCUSED_COLOR 220,220,220
+#define DEFAULT_BACKGROUND_DRAGGED_COLOR DEFAULT_BACKGROUND_SELECTED_COLOR 
+#define DEFAULT_TEXT_COLOR 0,0,0
+#define DEFAULT_FONT "arial.ttf"
+
+#define BACKGROUND_COLOR "backgroundColor"
+#define FONT "font"
+#define TEXT_COLOR "text_color"
+
+#define DEFAULT_FONT_SIZE 8
 
 class TextFieldView : public UIView
 {
 public:
-	TextFieldView(ALLEGRO_FONT* font_, ALLEGRO_COLOR textColor_, TextUI* model_, WindowUI* interface_);
+	TextFieldView (TextUI* model_, WindowUI* interface_);
 	~TextFieldView();
 	void draw(void);
 
-private:
-	ALLEGRO_FONT* font;
-	ALLEGRO_COLOR backgroundColor;
-	ALLEGRO_COLOR textColor;
 };
