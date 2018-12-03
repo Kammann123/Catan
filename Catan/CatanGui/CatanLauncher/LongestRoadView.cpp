f746deddf6fb8436547029f3172b6755996da83c
Pablo Scheinfeld <scheinpablo@yahoo.com>
2018-12-03 10:28:59 -0800

    LongestRoadView

---
+++ Catan/CatanGui/CatanLauncher/LongestRoadView.cpp
@@ -0,0 +1,32 @@
+#include "LongestRoadView.h"
+#include <allegro5/allegro.h>
+
+LongestRoadView::LongestRoadView(LongestRoad * model_) : UIView(model_)
+{
+	images.clear();
+	images.setConfig("longest_road", LONGEST_ROAD_IMG);
+}
+
+LongestRoadView::~LongestRoadView()
+{
+}
+
+void LongestRoadView::draw(void)
+{
+	LongestRoad* longestRoad = (LongestRoad*)model;
+	ALLEGRO_BITMAP * btMap = nullptr;
+	if (model->getVisible()) {
+		/* Busco la imagen del LongestRoad */
+
+		if (images.has(LONGEST_ROAD_ID)) {
+			btMap = images[LONGEST_ROAD_ID].bitmap;
+		}
+		/*Dibujo la imagen*/
+		if (btMap != nullptr)
+			al_draw_bitmap(btMap, longestRoad->xPos(), longestRoad->yPos(),0);
+	}
+}
+
+void LongestRoadView::updateModelValues(void)
+{
+}
---
+++ Catan/CatanGui/CatanLauncher/LongestRoadView.h
@@ -0,0 +1,15 @@
+#include "../../CatanGame/LongestRoad.h"
+#include "../AllegroUI/UIView.h"
+
+
+#define LONGEST_ROAD_IMG		"CatanGui\\GUIDesigns\\GameMenu\\buildings\\raod.png"
+#define LONGEST_ROAD_ID			"longest_road"
+
+class LongestRoadView : public UIView
+{
+public:
+	LongestRoadView(LongestRoad* model_ = nullptr);
+	~LongestRoadView();
+	void draw(void);
+	void updateModelValues(void);
+};
