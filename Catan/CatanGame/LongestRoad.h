8b5af4041b759f4724446678bb73bab793b195cb
Lucas Kammann <lucaskammann@hotmail.com>
2018-12-03 09:51:07 -0300

    Added LongestRoad interface. But hasn't been defined yet.

---
+++ Catan/CatanGame/LongestRoad.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../CatanGui/AllegroUI/FrameUI.h"
+
+/*
+* LongestRoad 
+* Clase modelo con la informacion de quien
+* posee el longest road, y la informacion
+* para su representacion grafica
+*/
+class LongestRoad : public FrameUI {
+public:
+
+	/*
+	* LongestRoad - Se construye inicialmente
+	* sin mas informacion que una referencia al juego
+	* para conocer la ubicacion de su posicion defecto
+	*/
+	LongestRoad(CatanGame* game);
+
+	/*
+	* assign - Se asigna la carta a un jugador
+	* y en dicho proceso se cambia su ubicacion.
+	* 
+	* Por defecto nullptr indica que se remueve.
+	*/
+	void assign(Player* player = nullptr);
+
+	/*
+	* who - Permite conocer a quien pertenece
+	*/
+	PlayerId who(void) const;
+
+private:
+	Player * player;
+};
