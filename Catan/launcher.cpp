f8503ea6974079f53cf0c77502426f8561d5386d
Joaquin Gaytan <jgaytan@itba.edu.ar>
2018-12-05 00:04:16 -0300

    github lpm vol2

--- Catan/launcher.cpp
+++ Catan/launcher.cpp
@@ -11,9 +11,6 @@ int main(int argc, char** argv) {
 
 		CatanLauncher launcher;
 
-		launcher.getContext().getGame().getCatanMap()->generateLand();
-		launcher.getContext().getGame().getCatanMap()->generateTokens();
-		auto blas = launcher.getContext().getGame().getCatanMap()->getMap();
 
 		/*
 		* Verifico que no haya terminado y,
