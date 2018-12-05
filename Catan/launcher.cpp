b8c565e6bffb666d8831367baebec125639911c6
Pablo Scheinfeld <scheinpablo@yahoo.com>
2018-12-04 19:05:46 -0800

    shs

--- Catan/launcher.cpp
+++ Catan/launcher.cpp
@@ -9,12 +9,7 @@ int main(int argc, char** argv) {
 
 	if (WindowUI::isAllegroInit()) {
 
-		CatanLauncher launcher;
-
-		launcher.getContext().getGame().getCatanMap()->generateLand();
-		launcher.getContext().getGame().getCatanMap()->generateTokens();
-		auto blas = launcher.getContext().getGame().getCatanMap()->getMap();
-
+		CatanLauncher launcher;	
 		/*
 		* Verifico que no haya terminado y,
 		* cerrado... y bueno, eso es todo!
