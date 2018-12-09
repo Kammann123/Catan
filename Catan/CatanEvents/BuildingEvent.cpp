dbad58c405380bdabe743380a1da181a4e51d6c7
Lucas Kammann <lucaskammann@hotmail.com>
2018-12-09 00:26:40 -0300

    Fixed

--- Catan/CatanEvents/BuildingEvent.cpp
+++ Catan/CatanEvents/BuildingEvent.cpp
@@ -1,7 +1,17 @@
 #include "BuildingEvent.h"
 
 BuildingEvent::
-BuildingEvent(BuildingPacket* packet) : CatanEvent(Events::BUILDING, Sources::NETWORKING, PlayerId::PLAYER_TWO), BuildingData(packet->getType(), packet->getCoords()) {}
+BuildingEvent(BuildingPacket* packet) : CatanEvent(Events::BUILDING, Sources::NETWORKING, PlayerId::PLAYER_TWO), BuildingData(packet->getType(), packet->getCoords()) {
+	switch (this->type) {
+		case BuildingType::CITY:
+		case BuildingType::SETTLEMENT:
+			this->coords.forceDot();
+			break;
+		case BuildingType::ROAD:
+			this->coords.forceEdge();
+			break;
+	}
+}
 
 BuildingEvent::
 BuildingEvent(Coord coords, BuildingType type, PlayerId player) : CatanEvent(Events::BUILDING, Sources::GUI, player), BuildingData(type, coords) {
