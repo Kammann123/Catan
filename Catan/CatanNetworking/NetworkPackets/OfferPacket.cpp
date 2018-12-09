ede4651add2ed772ef11fad81512c3309a9fbfba
Lucas Kammann <lucaskammann@hotmail.com>
2018-12-09 01:00:53 -0300

    I can't believe this.

--- Catan/CatanNetworking/NetworkPackets/OfferPacket.cpp
+++ Catan/CatanNetworking/NetworkPackets/OfferPacket.cpp
@@ -25,6 +25,7 @@ OfferPacket::getDataStream(unsigned int& length) {
 	unsigned char* buff = new unsigned char[bufferLength];
 	
 	/* Armo y guardo paquete */
+	length = bufferLength;
 	buff[0] = (unsigned char)this->getHeader();
 	buff[1] = (unsigned char)givenResources.size();
 	buff[2] = (unsigned char)receivedResources.size();
